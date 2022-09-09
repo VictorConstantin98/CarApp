1ce48de0c7ecbc1b003071f7081825b3f1c8deca
Costi <sova.victor98@gmail.com>
2022-09-09 11:47:13 +0300

    Exercitii masina

---
+++ CarApp/CarApp/Car.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarApp
+{
+    internal class Car
+    {
+        // Atribute
+        public string brand;
+        public string model;
+        public float motorizare;
+        public long sasiu;
+        public string collor;
+        public bool headupdisplay;
+
+        // Constructori
+        // Constructor implicit
+        public Car()
+        {
+            brand = "BMW";
+            sasiu = 223311;
+        }
+        // Constructor explicit partial
+        public Car(string brand, long sasiu, bool headupdisplay)
+        {
+            this.brand = brand;
+            this.sasiu = sasiu;
+            this.headupdisplay = headupdisplay;
+        }
+        // Constructor explicit
+        public Car(string brand, string model, int motorizare, long sasiu, string collor, bool headupdisplay)
+        {
+            this.brand = brand;
+            this.model = model;
+            this.motorizare = motorizare;
+            this.sasiu = sasiu;
+            this.collor = collor;
+            this.headupdisplay = true;
+        }
+        // Functie
+        public void writeToConsole()
+        {
+            Console.WriteLine("Brand: " + brand);
+            Console.WriteLine("Model: " + model);
+            Console.WriteLine("Motorizare: " + motorizare);
+            Console.WriteLine("Sasiu: " + sasiu);
+            Console.WriteLine("Culoare: " + collor);
+            Console.WriteLine("HeadUp Display: " + headupdisplay);
+        }
+    }
+}
---
+++ CarApp/CarApp/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace CarApp
+{
+    public class Program
+    {
+        public static void Main (string[]args)
+        {
+            Car car1 = new Car();
+            Console.WriteLine(car1.brand);
+            car1.sasiu = 223311;
+            Console.WriteLine(car1.sasiu);
+           
+            Car car2 = new Car("Audi", 222111, true);
+            car2.model = "A3";
+            car2.motorizare = 2000;
+            car2.collor = "Rosu";
+            car2.writeToConsole();
+
+            Car car3 = new Car("Mercedes", "S-Klasse", 2200, 13245, "Negru", true);
+            car3.writeToConsole();
+
+            Car car4 = new Car("Volkswagen", 278911, false);
+            car4.writeToConsole();
+
+            Car car5 = new Car("Toyota", "RaV", 1800, 888192, "Verde", true);
+            car5.writeToConsole();
+
+            Car car6 = new Car();
+            car6.brand = "Lamborghini";
+            Console.WriteLine(car6.brand);
+            car6.model = "Huracan";
+            Console.WriteLine(car6.model);
+            car6.sasiu = 888111;
+            Console.WriteLine(car6.sasiu);
+            car6.motorizare = 5000;
+            Console.WriteLine(car6.motorizare);
+            car6.collor = "Mov";
+            Console.WriteLine(car6.collor);
+            car6.headupdisplay = true;
+            Console.WriteLine(car6.headupdisplay);
+
+            Car car7 = new Car("Hyundai", 819021, true);
+            car7.model = "Tucson";
+            car7.motorizare = 1600;
+            car7.collor = "Alb";
+            car7.writeToConsole();
+
+            Car car8 = new Car("Range Rover", "Evoque", 3200, 777123, "Gri", true);
+            car8.writeToConsole();
+
+            Car car9 = new Car("Dacia", "Logan", 1000, 888000, "Alb", false);
+            car9.writeToConsole();
+
+            Car car10 = new Car("Renault", 810023, false);
+            car10.model = "Eclipse";
+            car10.motorizare = 1200;
+            car10.collor = "Galben";
+            car10.writeToConsole();
+
+
+
+
+            
+        }
+    }
+}
